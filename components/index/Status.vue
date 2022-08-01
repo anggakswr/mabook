7f51c30b27538d57c4fd81cc63ea8c9b6feaddba
Kamiswara Angga W <kamiswara98@gmail.com>
2022-08-01 11:23:50 +0700

    Display status data from API

--- components/index/Status.vue
+++ components/index/Status.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="mb-[20px] rounded-lg bg-gray-700 p-[10px]">
-    <IndexStatusHeader />
+    <IndexStatusHeader :id="post.id" />
 
-    <p class="text-xl my-[10px]">tes status</p>
+    <p class="text-xl my-[10px]">{{ post.title }}</p>
 
     <hr class="my-[10px] border-t border-gray-600" />
 
@@ -40,5 +40,11 @@
 <script>
 export default {
   name: 'IndexStatus',
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -12,10 +12,21 @@
         <IndexBtns />
       </div>
 
+      <!-- statuses skeletons (loading placeholder / loading indicator) -->
+      <template v-if="loading">
+        <IndexStatusSkeleton />
+        <IndexStatusSkeleton />
+        <IndexStatusSkeleton />
+      </template>
+
       <!-- stasuses -->
-      <IndexStatus />
-      <IndexStatus />
-      <IndexStatus />
+      <template v-else>
+        <IndexStatus
+          v-for="(post, index) in posts"
+          :key="'post-' + post.id + index"
+          :post="post"
+        />
+      </template>
     </section>
   </div>
 </template>
@@ -23,5 +34,33 @@
 <script>
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      loading: false,
+      posts: [],
+    }
+  },
+  async fetch() {
+    await this.getPosts()
+  },
+  methods: {
+    async getPosts() {
+      this.loading = true
+
+      try {
+        const res = await this.$axios.get(
+          'https://jsonplaceholder.typicode.com/posts'
+        )
+
+        // console.log('getPosts res.data', res.data)
+
+        this.posts = res.data
+      } catch {
+        // show err popup
+      }
+
+      this.loading = false
+    },
+  },
 }
 </script>
